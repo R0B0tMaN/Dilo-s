f4fea301c668c04884807b5ebff66f81f3ec156c
Fahd <160652744+R0B0tMaN@users.noreply.github.com>
2025-02-14 11:48:10 +0100

    Add files via upload

---
+++ Divine/script.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+    const message = document.getElementById('message');
+    const subMessage = document.getElementById('subMessage');
+
+    setTimeout(() => {
+        subMessage.style.opacity = 1;
+    }, 2000);
+};
